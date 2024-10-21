d7b89c9d9fbe19636d2ebe1d9ff3a01dcba8398e
McRodoku <126914671+McRodoku@users.noreply.github.com>
2024-10-21 14:40:10 -0600

    Update sketch.js

--- Homework 9/js/sketch.js
+++ Homework 9/js/sketch.js
@@ -6,9 +6,16 @@ function setup()
 
 function draw()
 {
-  background(150, 75, 200)
-  fill(200, 100, 50)
-  circle(75, 75, 100)
-  circle(325, 75, 100)
-  rect(20, 350, 300, 150)
+  background(150, 75, 200);
+  fill(200, 100, 50);
+  circle(100, 75, 100);
+  circle(325, 75, 100);
+  fill(100, 50, 256);
+  rect(80, 350, 300, 150);
+  fill(134, 76, 2);
+  triangle(150,325,225,200,280,320);
+  line(100,7,175,50);
+  line(200,55,400,5);
+  point(250,90);
+  
 }
