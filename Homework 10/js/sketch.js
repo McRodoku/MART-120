0370aa6a614407b120f4248b4f60b6c53584d8e3
McRodoku <126914671+McRodoku@users.noreply.github.com>
2024-10-27 15:21:51 -0600

    Update sketch.js

--- Homework 10/js/sketch.js
+++ Homework 10/js/sketch.js
@@ -1,9 +1,9 @@
 let x1 = 100, x2 = 325, y1 = 75, y2 = 75;
-let speedX1 = random(1, 3), speedX2 = random(1, 3);
+let speedX1 = 2, speedX2 = 1.5;
 let y3 = 350, y4 = 350;
-let speedY1 = random(1, 2), speedY2 = random(1, 2);
-let diagX = 80, diagY = 200;
-let diagSpeedX = random(1, 2), diagSpeedY = random(1, 2);
+let speedY1 = 1.2, speedY2 = 1.8;
+let diagX = 150, diagY = 325;
+let diagSpeedX = 1.3, diagSpeedY = 1.3;
 let textSizeVar = 22;
 let textGrowing = true;
 
@@ -16,6 +16,7 @@ function draw() {
 
   // Title text with oscillating size
   textSize(textSizeVar);
+  fill(0);
   text("How I feel about Mondays...", 110, 590);
   if (textGrowing) {
     textSizeVar += 0.5;
@@ -25,37 +26,37 @@ function draw() {
     if (textSizeVar <= 22) textGrowing = true;
   }
 
-  // Shapes moving along x-axis
+  // Shapes moving along the x-axis
   fill(200, 100, 50);
   circle(x1, y1, 100);
   circle(x2, y2, 100);
   x1 += speedX1;
   x2 += speedX2;
-  if (x1 > 400 || x1 < 100) speedX1 *= -1;
-  if (x2 > 400 || x2 < 100) speedX2 *= -1;
+  if (x1 > width - 50 || x1 < 50) speedX1 *= -1;
+  if (x2 > width - 50 || x2 < 50) speedX2 *= -1;
 
-  // Shapes moving along y-axis
+  // Shapes moving along the y-axis
   fill(255, 255, 255);
   rect(80, y3, 100, 150);
   rect(300, y4, 100, 150);
   y3 += speedY1;
   y4 += speedY2;
-  if (y3 > 450 || y3 < 350) speedY1 *= -1;
-  if (y4 > 450 || y4 < 350) speedY2 *= -1;
+  if (y3 > height - 200 || y3 < 200) speedY1 *= -1;
+  if (y4 > height - 200 || y4 < 200) speedY2 *= -1;
 
   // Shape moving diagonally
   fill(134, 76, 2);
   triangle(diagX, diagY, diagX + 75, diagY - 125, diagX + 130, diagY + 120);
   diagX += diagSpeedX;
   diagY += diagSpeedY;
-  if (diagX > 300 || diagX < 80) diagSpeedX *= -1;
-  if (diagY > 300 || diagY < 200) diagSpeedY *= -1;
+  if (diagX > width - 130 || diagX < 50) diagSpeedX *= -1;
+  if (diagY > height - 200 || diagY < 100) diagSpeedY *= -1;
 
   // Other static shapes and lines
+  strokeWeight(2);
   line(100, 7, 175, 50);
   line(200, 55, 400, 5);
   strokeWeight(10);
-  fill(0);
   point(100, 75);
   point(325, 75);
   point(250, 300);
