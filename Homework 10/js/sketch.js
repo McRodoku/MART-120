b19e4ca0a7c281427339ea2f70264332f4acb8dc
McRodoku <126914671+McRodoku@users.noreply.github.com>
2024-10-27 15:14:48 -0600

    Update sketch.js

--- Homework 10/js/sketch.js
+++ Homework 10/js/sketch.js
@@ -1,1 +1,31 @@
+function setup()
+{
+  createCanvas(500, 600)
+}
 
+
+function draw()
+{
+  background(150, 75, 200);
+  textSize(22)
+  text("How I feel about Mondays...", 110,590);
+  fill(200, 100, 50);
+  circle(100, 75, 100);
+  circle(325, 75, 100);
+  fill(255, 255, 255);
+  rect(80, 350, 300, 150);
+  fill(134, 76, 2);
+  triangle(150,325,225,200,280,320);
+  line(100,7,175,50);
+  line(200,55,400,5);
+  strokeWeight(10);
+  fill(0);
+  point(100,75);
+  point(325,75);
+  point(250,300);
+  point(200,300);
+  line(400,400,100,435);
+  fill(0);
+  textSize(25);
+  text("Chance Salois",150,550 );
+}
